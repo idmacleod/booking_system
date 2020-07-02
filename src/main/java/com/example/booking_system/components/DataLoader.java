838cd4b581ca279a4069b438af4daadd8377c5ec
idmacleod <idmacleod@gmail.com>
2020-07-02 10:36:28 +0100

    Models added, alongside repositories

--- src/main/java/com/example/booking_system/components/DataLoader.java
+++ src/main/java/com/example/booking_system/components/DataLoader.java
@@ -1,5 +1,6 @@
 package com.example.booking_system.components;
 
+import com.example.booking_system.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Controller;
@@ -7,8 +8,10 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class DataLoader implements ApplicationRunner {
 
+    @Autowired
+    BookingRepository bookingRepository;
 
-
+    @Au
 
 
 }
